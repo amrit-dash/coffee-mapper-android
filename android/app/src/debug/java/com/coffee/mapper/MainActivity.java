09d4da21cd313ea1e5ae9821daf3b1e8a1c3459b
Amrit Dash <amrit.dash60@gmail.com>
2026-06-29 18:44:36 +0530

    fix(android): share pre-warmed FlutterEngine in MainActivity
    Reuse CoffeeMapperApplication's engine so release and debug builds avoid
    initializing a second FlutterEngine at activity startup.

--- android/app/src/debug/java/com/coffee/mapper/MainActivity.java
+++ android/app/src/debug/java/com/coffee/mapper/MainActivity.java
@@ -1,6 +1,14 @@
 package com.coffee.mapper.dev;
 
+import android.content.Context;
+import androidx.annotation.NonNull;
+import com.coffee.mapper.CoffeeMapperApplication;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    public FlutterEngine provideFlutterEngine(@NonNull Context context) {
+        return ((CoffeeMapperApplication) getApplication()).getFlutterEngine();
+    }
 } 
--- android/app/src/main/java/com/coffee/mapper/MainActivity.java
+++ android/app/src/main/java/com/coffee/mapper/MainActivity.java
@@ -1,6 +1,13 @@
 package com.coffee.mapper;
 
+import android.content.Context;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    public FlutterEngine provideFlutterEngine(@NonNull Context context) {
+        return ((CoffeeMapperApplication) getApplication()).getFlutterEngine();
+    }
 } 
